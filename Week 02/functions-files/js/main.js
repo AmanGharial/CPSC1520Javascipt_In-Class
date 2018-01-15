4bf40b4a76f7f16fb7099af5503e92ead6feb23b
AmanGharial <amangharial@gmail.com>
2018-01-15 09:52:16 -0700

    week 02

---
+++ Week 02/functions-files/js/main.js
@@ -0,0 +1,19 @@
+/**
+ This is a comment block, enclosed in a slash + asterisk and ending with an asterisk + slash.
+ */
+
+ //Demo of calling a function - console is the javascript editor environment in the browser
+ //console.log(message);
+ console.log('main.js is loaded');
+
+ //The update innerHTML function has two paramerts:
+ // -selector - is a string that identifies which DOM element to get or select
+ // - newValue -  s string that will be the new value for the selector's .innserHTML
+ function updateInnerHTML(selector, newValue) {
+    document.querySelector(selector).innerHTML = newValue;
+  }
+
+  function strong(value) {
+    //combing string to produce a new string is calle concatenation.
+    return '<strong>' + value + '</strong>';
+  }
