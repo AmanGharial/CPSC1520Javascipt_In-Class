7f883c4b2171a5b41552496e0e151e0244eeca19
AmanGharial <AmanGharial@gmail.com>
2018-01-22 09:47:32 -0700

    featureImage.src = evt.target.href;
    second method to set the image src to the anchor's href value

--- Week 03/Exercise EventsFile/js/main.js
+++ Week 03/Exercise EventsFile/js/main.js
@@ -4,7 +4,11 @@ var featureLink = document.querySelector('a.feature.link');
 // 2. create the event listener function
 function featureLinkHandler(evt) {
     var featureImage = document.querySelector('img.feature');
-    featureImage.src = featureLink.href;
+
+    //featureImage.src = featureLink.href;   OR the below one 
+    featureImage.src = evt.target.href;
+
+
     featureImage.classList.remove('hidden');
     evt.preventDefault();
    }
